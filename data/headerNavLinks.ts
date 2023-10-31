533463ec45ae016b38d02497f9c8eadc31cc7674
maymoun.elfahim <amalou.free99@gmail.com>
2023-11-01 00:04:44 +0100

    First commit of portfolio

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/projects', title: 'Books' },
+  { href: '/services', title: 'Services' },
+  { href: '/about', title: 'About' },
+  { href: '/contact', title: 'Contact' },
+]
+
+export default headerNavLinks
---
+++ data/projectsData.ts
@@ -0,0 +1,40 @@
+const projectsData = [
+  {
+    title: 'Ego Is The Enemy',
+    description: `Ryan Holiday explores the detrimental impact of ego on success. 
+    The book highlights how unchecked ego can hinder personal and professional growth. 
+    It emphasizes the importance of humility, perseverance, and continuous learning. 
+    Through historical and contemporary examples, it illustrates the consequences of ego-driven behavior. Ultimately, 
+    it encourages readers to master their egos for long-term success and self-improvement.`,
+    imgSrc: '/static/images/egoIsThEnemy.jpg',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'La Vie Liquide',
+    description: `"La Vie liquide" est un livre du sociologue Zygmunt Bauman qui examine la nature évolutive et instable de la société contemporaine. 
+    L'auteur soutient que la modernité a créé une société "liquide" caractérisée par des relations et des identités flexibles et précaires. 
+    Il explore comment ces changements ont affecté la vie personnelle, le travail et la politique. 
+    Bauman encourage la réflexion sur la façon dont nous pouvons naviguer dans cette société liquide en recherchant des valeurs de solidarité et d'empathie.`,
+    imgSrc: '/static/images/LaVieLiquide.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Kafka: The Definitive Guide',
+    description: `What is Kafka, and how does it work? In this comprehensive e-book, 
+    you'll get full introduction to Apache Kafka® , the distributed, publish-subscribe queue for handling real-time data feeds.  
+    Learn how Kafka works, internal architecture, what it's used for, 
+    and how to take full advantage of Kafka stream processing technology.`,
+    imgSrc: '/static/images/kafka.jpg',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'Cracking the coding interview',
+    description: `Cracking the Coding Interview is here to help you through this process, 
+    teaching you what you need to know and enabling you to perform at your very best. 
+    I've coached and interviewed hundreds of software engineers.`,
+    imgSrc: '/static/images/crackingCodingInterview.png',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
